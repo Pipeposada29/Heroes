3466cf192e182db81f3cb5e4fd2b1253ee31b3e6
Pipeposada29 <pipeposada6@gmail.com>
2020-01-20 17:15:29 -0500

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import {HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
 import { HeroeComponent } from "./components/heroe/heroe.component";
+import {BuscadorComponent } from "./components/buscador/buscador.component";
 
 const APP_ROUTES: Routes=[
    { path:'home', component:HomeComponent},//Todo esto son rutas por las que navegamos
    { path:'about', component:AboutComponent},
    { path:'heroes', component:HeroesComponent},
    { path:'heroe/:id', component:HeroeComponent}, // La ruta heroe recibe un paramatro ID
+   { path:'buscar/:termino', component:BuscadorComponent},
    {path:'**', pathMatch: 'full',redirectTo:'home' }//Esta es la ruta que se va dirigir cuando  alla un error
 ];
 
---
+++ src/app/components/buscador/buscador.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+import { HeroesService } from "../servicios/heroes.service";
+
+@Component({
+  selector: 'app-buscador',
+  templateUrl: './buscador.component.html'
+})
+export class BuscadorComponent implements OnInit {
+
+  heroes:any [] = [];
+  termino:string;
+  constructor(private activateRouter: ActivatedRoute, private _heroesService:HeroesService) { }
+
+  ngOnInit() {
+
+    this.activateRouter.params.subscribe(params => {
+      this.termino = params['termino'];
+     
+      this.heroes = this._heroesService.buscarHeroe(params['termino']);
+      console.log(this.heroes);
+    })
+  }
+
+}
